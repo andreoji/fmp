7be4a28c97848b44556ee144b33bb0a37766a4ab
andre oji <andrewoji@gmail.com>
2017-04-04 19:55:50 +0100

    correcting some text

--- src/test/primes_test.hs
+++ src/test/primes_test.hs
@@ -2,7 +2,7 @@ import Primes
 import Test.HUnit
 
 test_empty_xs_with_ys = TestCase $ assertEqual "empty xs with ys" [2,4,6,8] (merge [] [2,4,6,8])
-test_xs_with_empty_ys = TestCase $ assertEqual "ys" [3,6,9,12] (merge [3,6,9,12] [])
+test_xs_with_empty_ys = TestCase $ assertEqual "xs with empty ys" [3,6,9,12] (merge [3,6,9,12] [])
 test_dedupes_xs_and_ys = TestCase $ assertEqual "dedupes xs and ys" [2,3,4,6,8,9,12] (merge [2,4,6,8] [3,6,9,12])
 test_merges_and_dedupes_with_x_as_head = TestCase $ assertEqual "merges two lists in order with x as head" [2,3,4,6,8,9,12] (xmerge [2,4,6,8] [3,6,9,12])
 
