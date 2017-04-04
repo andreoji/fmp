4aba407f1f3f8608ced63001d6da609cdc5e2ae5
andre oji <andrewoji@gmail.com>
2017-04-04 18:19:44 +0100

    takes n primes

--- src/lib/Primes.hs
+++ src/lib/Primes.hs
@@ -3,7 +3,18 @@ module Primes
   merge
 , xmerge
 , mergeAll
+, takeNPrimes
 ) where
+
+primes = 2:([3..] \\ composites)
+  where
+  composites = mergeAll [map (p*) [p..] | p <- primes]
+
+xs \\ [] = xs
+[] \\ ys = ys
+(x:xs) \\ (y:ys) | x<y = x:(xs \\ (y:ys))
+                 | x==y = xs \\ ys
+                 | x>y = (x:xs) \\ ys
 
 mergeAll :: Ord a => [[a]] -> [a]
 mergeAll (xs:xss) = xmerge xs (mergeAll xss)
@@ -18,3 +29,6 @@ merge (x:xs) (y:ys)
       | x>y = y:merge (x:xs) ys
 
 xmerge (x:xs) ys = x:merge xs ys
+
+takeNPrimes :: Int -> [Int]
+takeNPrimes n = take n primes
--- src/test/primes_test.hs
+++ src/test/primes_test.hs
@@ -7,3 +7,5 @@ test_dedupes_xs_and_ys = TestCase $ assertEqual "dedupes xs and ys" [2,3,4,6,8,9
 test_merges_and_dedupes_with_x_as_head = TestCase $ assertEqual "merges two lists in order with x as head" [2,3,4,6,8,9,12] (xmerge [2,4,6,8] [3,6,9,12])
 
 test_merges_list_of_lists = TestCase $ assertEqual "merges list of lists" [2,3,4,6,8,9,12,16] (mergeAll [[2,4,6,8], [3,6,9,12], [4,8,12,16]])
+
+test_takes_n_prime_numbers = TestCase $ assertEqual "takes n primes" [2,3,5,7,11] (takeNPrimes 5)
