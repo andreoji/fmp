e4ed520bbe0c139a33d48c325f177f517189bf15
andre oji <andrewoji@gmail.com>
2017-04-04 21:25:31 +0100

    adding main functions to run tests as suites

--- src/test/primes_test.hs
+++ src/test/primes_test.hs
@@ -9,3 +9,11 @@ test_merges_and_dedupes_with_x_as_head = TestCase $ assertEqual "merges two list
 test_merges_list_of_lists = TestCase $ assertEqual "merges list of lists" [2,3,4,6,8,9,12,16] (mergeAll [[2,4,6,8], [3,6,9,12], [4,8,12,16]])
 
 test_takes_n_prime_numbers = TestCase $ assertEqual "takes n primes" [2,3,5,7,11] (takeNPrimes 5)
+
+main :: IO Counts
+main = runTestTT $ TestList [test_empty_xs_with_ys
+                            , test_xs_with_empty_ys
+                            , test_dedupes_xs_and_ys
+                            , test_merges_and_dedupes_with_x_as_head
+                            , test_merges_list_of_lists
+                            , test_takes_n_prime_numbers] 
--- src/test/table_test.hs
+++ src/test/table_test.hs
@@ -4,3 +4,6 @@ import Test.HUnit
 five_by_five = [[0,2,3,5,7], [2,4,6,10,14],[3,6,9,15,21],[5,10,15,25,35],[7,14,21,35,49]]
 
 test_5_by_5_table = TestCase $ assertEqual "5 by 5" five_by_five (createTable 4)
+
+main :: IO Counts
+main = runTestTT $ TestList [test_5_by_5_table]
