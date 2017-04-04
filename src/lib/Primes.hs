ce05afce1e58b74284f194484afea6a4a1ee6749
andre oji <andrewoji@gmail.com>
2017-04-04 04:55:24 +0100

    merge function dedupes a y if equal to x

--- src/lib/Primes.hs
+++ src/lib/Primes.hs
@@ -3,4 +3,10 @@ module Primes
   merge
 ) where
 
-merge = "helloworld"
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs 
+merge [] ys = ys 
+merge (x:xs) (y:ys)
+      | x<y = x:merge xs (y:ys)
+      | x==y = x:merge xs ys 
+      | x>y = y:merge (x:xs) ys
--- src/test/primes_test.hs
+++ src/test/primes_test.hs
@@ -1,4 +1,7 @@
 import Primes
 import Test.HUnit
 
-test1 = TestCase $ assertEqual "the truth" "helloworld" (merge)
+test1 = TestCase $ assertEqual "empty xs with ys" [1,2,3] (merge [] [1,2,3])
+test2 = TestCase $ assertEqual "xs with ys" [4,5,6] (merge [4,5,6] [])
+test3 = TestCase $ assertEqual "xs with ys" [1,2,3,4,5,6] (merge [1,2,3] [4,5,6])
+test4 = TestCase $ assertEqual "dedupes when y = x" [1,2,3,4,5,6] (merge [1,2,3] [2,3,4,5,6])
