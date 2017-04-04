1ad6a04e7cfb8cd4871d72aa72316b0e315a5f7d
andre oji <andrewoji@gmail.com>
2017-04-04 19:51:08 +0100

    formatting of rows

--- src/lib/Formatting.hs
+++ src/lib/Formatting.hs
@@ -1,9 +1,14 @@
 module Formatting
 (
   join
+, format
 ) where
 
 import Data.List (intersperse)
 
 join :: [Int] -> String
 join nums = (concat . intersperse "| " . map show $ nums) ++ "|\n"
+
+format [] = []
+format ((0:xs):xss) = "|  | " ++ join xs ++ format(xss)
+format (xs:xss) = "| " ++ join xs ++ format(xss)
--- src/test/formatting_test.hs
+++ src/test/formatting_test.hs
@@ -2,3 +2,5 @@ import Formatting
 import Test.HUnit
 
 test_join = TestCase $ assertEqual "joins with a pipe" "2| 4| 6| 10| 14|\n"  (join [2,4,6,10,14])
+test_format_with_leading_zero = TestCase $ assertEqual "formats a row with leading zero" "|  | 2| 3| 5| 7|\n"  (format [[0, 2,3,5,7]])
+test_format_with_leading_n = TestCase $ assertEqual "formats a row with leading n" "| 2| 4| 6| 8| 10| 14|\n"  (format [[2,4,6,8,10,14]])
