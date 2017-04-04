80430c165dd311e811e8bb2d55d02597782dfcdf
andre oji <andrewoji@gmail.com>
2017-04-04 20:20:33 +0100

    function for IO

--- src/lib/Table.hs
+++ src/lib/Table.hs
@@ -1,13 +1,20 @@
 module Table
 (
   createTable
+, formatTable
 )
 where
 
 import Primes
+import Formatting
 
 primesTable n = [(0:primes)] ++ [map (p*) (1:primes) | p <- primes]
   where
   primes = takeNPrimes n
 
 createTable n = primesTable(n)
+
+formatTable n = putStrLn str
+  where
+  str = format t
+  t = createTable n
