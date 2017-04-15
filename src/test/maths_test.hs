86b047da36837d5d9dfc0253a2661b2c73d7bad9
andre oji <andrewoji@gmail.com>
2017-04-15 06:49:51 +0100

    failing test for empty xs with ys

--- src/test/maths_test.hs
+++ src/test/maths_test.hs
@@ -1,6 +1,8 @@
 import Test.HUnit
 import Maths
 test_xs_with_empty_ys = TestCase $ assertEqual "xs with empty ys" [2,3,5,7,11] ([2,3,5,7,11] \\ [])
+test_empty_xs_with_ys = TestCase $ assertEqual "xs with empty ys" [2,4,6,8,10] ([] \\ [2,4,6,8,10])
 
 main :: IO Counts
-main = runTestTT $ TestList [test_xs_with_empty_ys]
+main = runTestTT $ TestList [test_xs_with_empty_ys
+                            , test_empty_xs_with_ys]
